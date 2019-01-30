7df77d92d6460f7af65e76d50a2d1b05baf59d9e
Malith Ranasinghe <malithmp@gmail.com>
2019-01-30 00:16:21 -0500

    Add both channels and remove the offset

--- arduino_fc.ino
+++ arduino_fc.ino
@@ -11,22 +11,28 @@ volatile unsigned long int channel_2_period; // PPM signal period channel 2
 volatile unsigned long int channel_1_width;  // PPM signal width channel 1: should be 20ms
 volatile unsigned long int channel_2_width;  // PPM signal width channel 2: should be 20ms
 
+// Keep track of the lowest pin valus seen so we can calculate the diff
+volatile unsigned long int ch1_min;  // channel 1 lowest ppm period
+volatile unsigned long int ch2_min;  // channel 2 lowest ppm period
 
 void setup() {
   Serial.begin(9600);
   
   pin_2_start      = 0;
   pin_3_start      = 0;
-  channel_1_period = 0;
-  channel_2_period = 0;
+  channel_1_period = 999999;
+  channel_2_period = 999999;
   channel_1_width  = 0;
   channel_2_width  = 0;
 
+  ch1_min = 999999;
+  ch2_min = 999999;
+  
   pinMode(2, INPUT);
-  //pinMode(3, INPUT);
+  pinMode(3, INPUT);
     
   attachInterrupt(digitalPinToInterrupt(2), ch1_ppm_change, CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(3), ch2_ppm_change, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(3), ch2_ppm_change, CHANGE);
 
   Serial.println("begin");
 }
@@ -43,15 +49,44 @@ void ch1_ppm_change()  {
   }
 }
 
+void ch2_ppm_change()  { 
+  int x = digitalRead(3);
+  unsigned long int t = micros();
+  
+  if (x == HIGH) { // Rising edge
+    channel_2_width = t - pin_3_start;
+    pin_3_start = t;
+  }else{           // Falling edge
+    channel_2_period = t - pin_3_start;
+  }
+}
+
+void track_mins(){
+  if (ch1_min > channel_1_period) {
+     ch1_min = channel_1_period;
+  }
+
+  if (ch2_min > channel_2_period) {
+     ch2_min = channel_2_period;
+  }
+}
+
+void mix_signals() {
+  long int ch1 = channel_1_period - ch1_min;
+  long int ch2 = channel_2_period - ch2_min;
+
+  String chs="|CH1|:" + (String)ch1 + " |CH2|:" + (String)ch2;
+  Serial.println(chs); 
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(100);
+  track_mins();
+  mix_signals();
+  //String ch1="|CH1| period:" + (String)channel_1_period + " width:" + (String)channel_1_width + " min:" + (String)ch1_min;
+  //String ch2="|CH2| period:" + (String)channel_2_period + " width:" + (String)channel_2_width + " min:" + (String)ch2_min;
   
-  String ch1="|CH1| period:" + (String)channel_1_period + " width:" + (String)channel_1_width;
-  
-  Serial.println(ch1);
-
-  delay(100);
-  
+  //Serial.println(ch1); 
+  //Serial.println(ch2);
+  delay(500);
  }

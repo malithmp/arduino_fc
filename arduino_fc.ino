c10972dc5443448ef73f37ca0c89b5915f6c9ba4
Malith Ranasinghe <malithmp@gmail.com>
2019-02-04 00:23:08 -0500

    Read both signals and map them to a range

--- arduino_fc.ino
+++ arduino_fc.ino
@@ -2,35 +2,26 @@
 const byte INTERRUPT_1 = 2;
 const byte INTERRUPT_2 = 3;
 
-volatile unsigned long int pin_2_start;  // When channel 1 went high
-volatile unsigned long int pin_3_start;  // When channel 2 went high
+struct Channel {
+  volatile unsigned long int pulse_start;
+  int pulse_width;
+  int pulse_width_max;
+  int pulse_width_min;
+  
+  int pulse_period; // 20ms for FS-iA6
+};
 
-volatile unsigned long int channel_1_period; // PPM signal period channel 1
-volatile unsigned long int channel_2_period; // PPM signal period channel 2
-
-volatile unsigned long int channel_1_width;  // PPM signal width channel 1: should be 20ms
-volatile unsigned long int channel_2_width;  // PPM signal width channel 2: should be 20ms
-
-// Keep track of the lowest pin valus seen so we can calculate the diff
-volatile unsigned long int ch1_min;  // channel 1 lowest ppm period
-volatile unsigned long int ch2_min;  // channel 2 lowest ppm period
+// PPM signals go from 1000 - 1900. so a safe min is 1200, safe max is 1500 and a safe starting point is somewhere in the middle 1350.
+// We use these values for pulse_width, pulse_width_min and pulse_width_max to initialize
+Channel ch1 = {0, 1350, 1500, 1200, 20000};
+Channel ch2 = {0, 1350, 1500, 1200, 20000};
 
 void setup() {
   Serial.begin(9600);
-  
-  pin_2_start      = 0;
-  pin_3_start      = 0;
-  channel_1_period = 999999;
-  channel_2_period = 999999;
-  channel_1_width  = 0;
-  channel_2_width  = 0;
-
-  ch1_min = 999999;
-  ch2_min = 999999;
-  
+   
   pinMode(2, INPUT);
   pinMode(3, INPUT);
-    
+  
   attachInterrupt(digitalPinToInterrupt(2), ch1_ppm_change, CHANGE);
   attachInterrupt(digitalPinToInterrupt(3), ch2_ppm_change, CHANGE);
 
@@ -42,10 +33,10 @@ void ch1_ppm_change()  {
   unsigned long int t = micros();
   
   if (x == HIGH) { // Rising edge
-    channel_1_width = t - pin_2_start;
-    pin_2_start = t;
+    ch1.pulse_period = t - ch1.pulse_start;
+    ch1.pulse_start  = t;
   }else{           // Falling edge
-    channel_1_period = t - pin_2_start;
+    ch1.pulse_width = t -ch1.pulse_start;
   }
 }
 
@@ -54,39 +45,43 @@ void ch2_ppm_change()  {
   unsigned long int t = micros();
   
   if (x == HIGH) { // Rising edge
-    channel_2_width = t - pin_3_start;
-    pin_3_start = t;
+    ch2.pulse_period = t - ch2.pulse_start;
+    ch2.pulse_start  = t;
   }else{           // Falling edge
-    channel_2_period = t - pin_3_start;
+    ch2.pulse_width = t -ch2.pulse_start;
   }
 }
 
-void track_mins(){
-  if (ch1_min > channel_1_period) {
-     ch1_min = channel_1_period;
-  }
-
-  if (ch2_min > channel_2_period) {
-     ch2_min = channel_2_period;
-  }
+void track_min_max(Channel *ch){
+  if (ch->pulse_width_min > ch->pulse_width) { ch->pulse_width_min = ch->pulse_width; } // Track min
+  if (ch->pulse_width_max < ch->pulse_width) { ch->pulse_width_max = ch->pulse_width; } // Track max
 }
 
-void mix_signals() {
-  long int ch1 = channel_1_period - ch1_min;
-  long int ch2 = channel_2_period - ch2_min;
+// Normalize width between given min max values 
+int normalized_width(Channel *ch, int min_val, int max_val) {
+  track_min_max(ch);
+  return map(ch->pulse_width, ch->pulse_width_min, ch->pulse_width_max, min_val, max_val);
+  //return  (int) ( (float)(ch->pulse_width - ch->pulse_width_min) * 100 / (float) (ch->pulse_width_max - ch->pulse_width_min)  );
+}
 
-  String chs="|CH1|:" + (String)ch1 + " |CH2|:" + (String)ch2;
-  Serial.println(chs); 
+void print_debug(){
+   String chn1="|CH1| period:" + (String)ch1.pulse_period + " width:" + (String)ch1.pulse_width + " min:" + (String)ch1.pulse_width_min + " max:" + (String)ch1.pulse_width_max ;
+   String chn2="|CH2| period:" + (String)ch2.pulse_period + " width:" + (String)ch2.pulse_width + " min:" + (String)ch2.pulse_width_min + " max:" + (String)ch2.pulse_width_max ;
+   Serial.println(chn1); 
+   Serial.println(chn2); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  track_mins();
-  mix_signals();
-  //String ch1="|CH1| period:" + (String)channel_1_period + " width:" + (String)channel_1_width + " min:" + (String)ch1_min;
-  //String ch2="|CH2| period:" + (String)channel_2_period + " width:" + (String)channel_2_width + " min:" + (String)ch2_min;
+  int ch1_norm = normalized_width(&ch1, -100, 100);
+  int ch2_norm = normalized_width(&ch2, 0   , 100);
   
-  //Serial.println(ch1); 
-  //Serial.println(ch2);
-  delay(500);
+  Serial.println("CH1: " + (String)ch1_norm + "  CH2: " + (String)ch2_norm);
+  
+  //mix_signals();
+
+  // Debug and find channel mins and maxes
+  // print_debug();
+  
+  delay(100);
  }
